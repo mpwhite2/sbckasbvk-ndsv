c6a3ac70a65cd2c2c81b333e79fa2fc42bb38cd1
mpwhite2 <110784371+mpwhite2@users.noreply.github.com>
2023-02-21 14:52:32 -0800

    Update pxt.json, main.blocks, main.ts, tilemap.g.jres, tilemap.g.ts, images.g.jres, images.g.ts

--- main.ts
+++ main.ts
@@ -1,9 +1,34 @@
+namespace SpriteKind {
+    export const Wasp = SpriteKind.create()
+}
 function CreateWasp (C: number, R: number) {
-    Wasp = sprites.create(assets.image`Wasp`, SpriteKind.Enemy)
+    Wasp = sprites.create(assets.image`Wasp`, SpriteKind.Wasp)
     tiles.placeOnTile(Wasp, tiles.getTileLocation(C, R))
-    game.onUpdateInterval(200, function() { 
-        story.spriteMoveToLocation(Wasp, TargetX, TargetY, 70)
-    })
+    Wasp.follow(mySprite, 50)
+    characterAnimations.loopFrames(
+    Wasp,
+    assets.animation`Flying up or down`,
+    100,
+    characterAnimations.rule(Predicate.MovingUp)
+    )
+    characterAnimations.loopFrames(
+    Wasp,
+    assets.animation`Flying up or down`,
+    100,
+    characterAnimations.rule(Predicate.MovingDown)
+    )
+    characterAnimations.loopFrames(
+    Wasp,
+    assets.animation`Flying left or right`,
+    500,
+    characterAnimations.rule(Predicate.MovingRight)
+    )
+    characterAnimations.loopFrames(
+    Wasp,
+    assets.animation`Flying left or right`,
+    500,
+    characterAnimations.rule(Predicate.MovingLeft)
+    )
 }
 function setDir (dir: boolean) {
     u = false
@@ -46,31 +71,50 @@ controller.down.onEvent(ControllerButtonEvent.Pressed, function () {
     setDir(d)
     d = true
 })
+info.onLifeZero(function () {
+    game.gameOver(false)
+})
+sprites.onOverlap(SpriteKind.Projectile, SpriteKind.Wasp, function (sprite, otherSprite) {
+    sprites.destroy(sprite)
+    sprites.destroy(otherSprite, effects.disintegrate, 500)
+    info.changeScoreBy(1)
+})
+sprites.onOverlap(SpriteKind.Player, SpriteKind.Wasp, function (sprite, otherSprite) {
+    sprites.destroy(otherSprite)
+    info.changeLifeBy(-1)
+    PlayerHealth.value += -1
+})
 let location: tiles.Location = null
 let projectile: Sprite = null
 let dir = false
 let l = false
 let r = false
 let u = false
+let Wasp: Sprite = null
+let PlayerHealth: StatusBarSprite = null
 let mySprite: Sprite = null
 let d = false
-let TargetY = 0
+info.setLife(100)
 let TargetX = 0
-let Wasp: Sprite = null
-let WaspRate = 200
+let TargetY = 0
+let WaspRate = 1000
 d = true
 color.setPalette(
 color.GrayScale
 )
 color.startFadeFromCurrent(color.Arcade, 500)
 if (game.ask("Input a name?") || !(blockSettings.exists("Name"))) {
-    blockSettings.writeString("Name", game.askForString("Please input a name"))
+    blockSettings.writeString("Name", game.askForString("Please input a name", 12))
 }
 color.setPalette(
 color.GrayScale
 )
 color.startFadeFromCurrent(color.Arcade, 500)
 mySprite = sprites.create(assets.image`Front`, SpriteKind.Player)
+PlayerHealth = statusbars.create(16, 2, StatusBarKind.Health)
+PlayerHealth.max = 100
+PlayerHealth.attachToSprite(mySprite)
+PlayerHealth.setColor(4, 13)
 tiles.setCurrentTilemap(tilemap`Enter`)
 controller.moveSprite(mySprite)
 scene.cameraFollowSprite(mySprite)
@@ -98,11 +142,12 @@ assets.animation`Right`,
 100,
 characterAnimations.rule(Predicate.MovingRight)
 )
-game.onUpdateInterval(500, function () {
+game.onUpdateInterval(WaspRate, function () {
     location = tiles.getTilesByType(assets.tile`myTile0`)._pickRandom()
     CreateWasp(location.column, location.row)
 })
-game.onUpdateInterval(200, function () {
-    TargetX = mySprite.x + randint(-20, 20)
-    TargetY = mySprite.y + randint(-20, 20)
+game.onUpdateInterval(2000, function () {
+    if (info.life() < 100) {
+        info.changeLifeBy(1)
+    }
 })
